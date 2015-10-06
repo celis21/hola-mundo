7e1404eb6bcf8e78191ae6fe67df590e92a3fc1e
15 <15@I15>
2015-10-06 07:58:11 -0500

    andres celis base de datos modificada

---
+++ basmodificada/src/com/example/basmodificada/AdminSQLiteOpenHelper.java
@@ -0,0 +1,17 @@
+package com.example.basmodificada;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class AdminSQLiteOpenHelper {
+
+	public AdminSQLiteOpenHelper(MainActivity mainActivity, String string,
+			Object object, int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SQLiteDatabase getWritableDatabase() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
